5c7bc0fa29a888545dc6cbd704f1e32858e36243
amalker <it.amalker@yandex.ru>
2020-04-02 17:50:23 +0300

    add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+
+import * as actions from '../actions';
+
+const messages = handleActions({
+  [actions.initStateSuccess](state, { payload: { gon } }) {
+    return {
+      messages: gon.messages,
+    };
+  },
+  [actions.createMessageSuccess](state, { payload: { message } }) {
+    return {
+      messages: [...state.messages, message.attributes],
+    };
+  },
+}, { messages: [] });
+
+const channels = handleActions({
+  [actions.initStateSuccess](state, { payload: { gon } }) {
+    return {
+      channels: gon.channels,
+      currentChannelId: gon.currentChannelId,
+    };
+  },
+  [actions.getChannelsSuccess](state, { payload: { data } }) {
+    const channels = data.map(({ attributes }) => ({ ...attributes }));
+    return {
+      channels,
+    };
+  },
+}, { channels: [], currentChannelId: 1 });
+
+export default combineReducers({
+  channels,
+  messages,
+});
