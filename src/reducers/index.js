50b4293cfc60365701acbdd7d725d10bbde8c2a0
amalker <it.amalker@yandex.ru>
2020-04-05 18:04:20 +0300

    add states to channelUI and to creation channel/message loogic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,30 @@ import { handleActions } from 'redux-actions';
 
 import * as actions from '../actions';
 
+const messageCreateState = handleActions({
+  [actions.createMessageRequest]() {
+    return 'requested';
+  },
+  [actions.createMessageFailure]() {
+    return 'failed';
+  },
+  [actions.createMessageSuccess]() {
+    return 'finished';
+  },
+}, 'none');
+
+const channelCreateState = handleActions({
+  [actions.createChannelRequest]() {
+    return 'requested';
+  },
+  [actions.createChannelFailure]() {
+    return 'failed';
+  },
+  [actions.createChannelSuccess]() {
+    return 'finished';
+  },
+}, 'none');
+
 const messages = handleActions({
   [actions.initStateSuccess](state, { payload: { gon } }) {
     return {
@@ -23,6 +47,12 @@ const channels = handleActions({
       currentChannelId: gon.currentChannelId,
     };
   },
+  [actions.createChannelSuccess](state, { payload: { channel } }) {
+    return {
+      ...state,
+      channels: [...state.channels, channel.attributes],
+    };
+  },
   [actions.getChannelsSuccess](state, { payload: { data } }) {
     const channels = data.map(({ attributes }) => ({ ...attributes }));
     return {
@@ -37,7 +67,20 @@ const channels = handleActions({
   },
 }, { channels: [], currentChannelId: 1 });
 
+const channelsUI = handleActions({
+  [actions.newChannelModalShow](state, { payload: { modalShow } }) {
+    console.log('modalShow ', modalShow)
+    return {
+      ...state,
+      addNewChannelShow: modalShow, 
+    };
+  },
+}, { addNewChannelShow: false });
+
 export default combineReducers({
   channels,
   messages,
+  channelsUI,
+  messageCreateState,
+  channelCreateState,
 });
