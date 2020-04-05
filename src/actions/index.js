4792a2af6a58836ea63d1f5bb124c461ddb4309f
amalker <it.amalker@yandex.ru>
2020-04-05 18:02:16 +0300

    add different states to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,17 +2,42 @@ import axios from 'axios';
 import { createAction } from 'redux-actions';
 import routes from '../routes.js';
 
-export const createMessageSuccess = createAction('MESSAGE_CREATE_SUCCESS');
-
 export const initStateSuccess = createAction('STATE_INIT');
+
+export const newChannelModalShow = createAction('CHANNEL_MODAL_SHOW');
+
+export const createMessageRequest = createAction('MESSAGE_CREATE_REQUEST');
+export const createMessageSuccess = createAction('MESSAGE_CREATE_SUCCESS');
+export const createMessageFailure = createAction('MESSAGE_CREATE_FAILURE');
+
+export const createChannelRequest = createAction('CHANNEL_CREATE_REQUEST');
+export const createChannelSuccess = createAction('CHANNEL_CREATE_SUCCESS');
+export const createChannelFailure = createAction('CHANNEL_CREATE_FAILURE');
 
 export const getChannelsSuccess = createAction('CHANNELS_GET_SUCCESS');
 export const switchChannelsSuccess = createAction('CHANNELS_SWITCH_SUCCESS');
 
 export const createMessage = ({ text, author, currentChannelId }) => async (dispatch) => {
-  const url = routes.channelMessagesPath(currentChannelId);
-  const response = await axios.post(url, { data: { attributes: { text, author } } });
-  dispatch(createMessageSuccess({ message: response.data.data }));
+  dispatch(createMessageRequest());
+  try {
+    const url = routes.channelMessagesPath(currentChannelId);
+    await axios.post(url, { data: { attributes: { text, author } } });
+  } catch (e) {
+    dispatch(createMessageFailure());
+    throw new Error(`Cannot create new message, probably network problems: ${e}`);
+  }
+};
+
+export const createChannel = ({ name }) => async (dispatch) => {
+  dispatch(createChannelRequest());
+  try {
+    const url = routes.channelsPath();
+    await axios.post(url, { data: { attributes: { name } } });
+  } catch (e) {
+    dispatch(createChannelFailure());
+    throw new Error(`Cannot add new channel, probably network problems: ${e}`);
+  }
+  
 };
 
 export const getChannels = () => async (dispatch) => {
