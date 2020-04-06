8387748133b4c3003cd4e79cc3abf178c2128f72
amalker <it.amalker@yandex.ru>
2020-04-06 18:31:43 +0300

    rename component, refactor

---
+++ src/components/ModalCreateChannel.jsx
@@ -0,0 +1,64 @@
+// @ts-check
+
+import React, { useRef } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from '../actions/index.js';
+import { useFormik } from 'formik';
+
+const modalCreateChannel = () => {
+  const dispatch = useDispatch();
+  // @ts-ignore
+  const channelStateUI = useSelector((state) => state.channelsUI);
+  const { addNewChannelShow: show } = channelStateUI;
+
+  const formControlEl = useRef(null);
+
+  const handleClose = () => dispatch(actions.newChannelModalShow({ modalShow: false }));
+  const handleShow = () => {
+    dispatch(actions.newChannelModalShow({ modalShow: true }));
+    setTimeout(()=> formControlEl.current.focus(), 200);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      text: '',
+    },
+    onSubmit: ({ text: name }, { resetForm }) => {
+      dispatch(actions.createChannel({ name }));
+      resetForm();
+    },
+  });
+
+  return (
+    <>
+      <Button className="ml-auto btn-sm" variant="success" onClick={handleShow}>
+        +
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new channel</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={formik.handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Channel name:</Form.Label>
+              <Form.Control
+                ref={formControlEl}
+                type="text"
+                name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            <Button variant="secondary" type="submit" onClick={handleClose}>
+              Add new channel
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default modalCreateChannel;
