52c3f82e32808be13eb70b38c56318be1652d760
amalker <it.amalker@yandex.ru>
2020-04-09 18:24:21 +0300

    add smooth botom scroll on new message, fix eslint warnings

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
 // @ts-check
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import socket from '../socket';
 import MessagesStatus from './MessagesStatus';
@@ -16,14 +16,22 @@ const Chat = () => {
   const messagesState = useSelector((state) => state.messages);
   const messages = messagesState.messages.filter((m) => m.channelId === currentChannelId);
 
+  const messagesEnd = useRef(null);
+
   useEffect(() => {
     socket.on('newMessage', ({ data }) => {
       dispatch(actions.updateMessages({ message: data }));
     });
-  }, []);
+  }, [dispatch]);
+
+  const scrollToBottom = () => {
+    messagesEnd.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(scrollToBottom, [messages]);
 
   const renderMessages = () => (
-    <div id="messages-box" className="chat-messages overflow-auto mb-1">
+    <div id="messages-box">
       {messages.map(({ id, author, text }) => (
         <div key={id}>
           <b>{author}</b>
@@ -37,7 +45,10 @@ const Chat = () => {
   return (
     <div className="col h-100">
       <div className="d-flex flex-column h-100">
-        {renderMessages()}
+        <div className="chat-messages overflow-auto mb-1">
+          {renderMessages()}
+          <div ref={messagesEnd} />
+        </div>
         <div className="mt-auto">
           <ChatField />
           <MessagesStatus messagesLength={messages.length} />
