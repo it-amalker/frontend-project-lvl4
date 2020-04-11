4e9054ac15e028069a7ba941752f836d1c3d8ffd
amalker <it.amalker@yandex.ru>
2020-04-11 14:11:18 +0300

    rename setSelected func

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@
 
 const setResetDelay = (f, ms) => setTimeout(f, ms);
 
-const setSelect = (el) => () => setTimeout(() => el.current.select(), 200);
+const setSelected = (el) => () => setTimeout(() => el.current.select(), 200);
 
-export { setResetDelay, setSelect };
+export { setResetDelay, setSelected };
