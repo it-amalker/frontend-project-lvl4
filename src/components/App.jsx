3e6f66afd6d7860e1cd8d77056d75e7b2e044618
amalker <it.amalker@yandex.ru>
2020-04-12 12:03:51 +0300

    fix pathes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import Channels from './Channels';
-import Chat from './Chat';
+import ChatBox from './ChatBox';
 
 const App = () => (
   <div className="row h-100 pb-3">
     <Channels />
-    <Chat />
+    <ChatBox />
   </div>
 );
 
