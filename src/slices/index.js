2341cb3569353f06e458a0f56b3953686dc7dd17
amalker <it.amalker@yandex.ru>
2020-04-13 11:50:05 +0300

    remove slice 'removeStatus'

--- src/slices/index.js
+++ src/slices/index.js
@@ -5,20 +5,17 @@ import { combineReducers } from '@reduxjs/toolkit';
 import channels, { actions as channelActions } from './channels';
 import messages, { actions as messagesActions } from './messages';
 import modalInfo, { actions as modalInfoActions } from './modalInfo';
-import removeStatus, { actions as removeStatusActions } from './removeStatus';
 
 export default combineReducers({
   channels,
   messages,
   modalInfo,
-  removeStatus,
 });
 
 const actions = {
   ...channelActions,
   ...messagesActions,
   ...modalInfoActions,
-  ...removeStatusActions,
 };
 
 export {
